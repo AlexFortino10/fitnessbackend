43e6a33eee5d47d8d3b48fd21a5cfef418362a6b
Alessandro <fortinoalessandro902@gmail.com>
2024-12-09 10:06:46 +0100

    Modificato app

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ async def generate_text(request: PromptRequest):
     payload = {
         "inputs": prompt,
         "parameters": {
-            "max_length": 50,  # Lunghezza maggiore per miglior contesto
-            "temperature": 0.7,
+            "max_length": 15,  # Lunghezza maggiore per miglior contesto
+            "temperature": 0.1,
             "top_k": 40,
             "top_p": 0.9,
             "do_sample": True,
